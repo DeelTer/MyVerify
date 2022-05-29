f3eaefd4a832715562581e334bab5bcf9aa7b970
DeelTer <roma200315@mail.ru>
2022-05-29 19:36:17 +0300

    Fix bug

--- src/main/java/ru/deelter/verify/player/DiscordPlayerAuthListener.java
+++ src/main/java/ru/deelter/verify/player/DiscordPlayerAuthListener.java
@@ -2,10 +2,8 @@ package ru.deelter.verify.player;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import ru.deelter.verify.Config;
 import ru.deelter.verify.MyVerify;
 import ru.deelter.verify.utils.Console;
@@ -23,14 +21,6 @@ public class DiscordPlayerAuthListener implements Listener {
 				player.setName(name);
 				Console.debug("Update user nickname " + name);
 			}
-		});
-	}
-
-	@EventHandler(priority = EventPriority.HIGH)
-	public void onQuit(PlayerQuitEvent event) {
-		Bukkit.getScheduler().runTaskAsynchronously(MyVerify.getInstance(), () -> {
-			DiscordPlayer player = DiscordPlayerManager.getByPlayer(event.getPlayer());
-			if (player.isLinked()) DiscordDatabase.updateInfo(player);
 		});
 	}
 }
